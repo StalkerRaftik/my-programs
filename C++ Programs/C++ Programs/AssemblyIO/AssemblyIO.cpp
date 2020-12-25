9774018a6baec7c826c178e156ba42d2d7b80f00
WhileTrueDoEnd <altair-ivan@ya.ru>
2020-12-25 19:01:01 +1000

    1.0

---
+++ C++ Programs/C++ Programs/AssemblyIO/AssemblyIO.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    
+    int a, x, y1, y2, y;
+    cin >> a >> x;
+    
+    __asm 
+    {
+        mov eax, a
+        mov ebx, x
+
+        cmp ebx, eax
+        jnle xbiggera
+        jmp else1
+
+    xbiggera:
+        cmp ebx, 0
+        jnl negend
+        
+        neg ebx
+        negend:
+
+        add eax, ebx
+        mov y1, eax // y1 готов
+        jmp ifend1
+    else1:
+        sub eax, 7
+        mov y1, eax
+    ifend1:
+        mov eax, a
+        mov ebx, x
+
+        cmp a, 3
+        jnle abiggerthree
+        jmp else2
+    abiggerthree:
+        mov ebx, 3
+        mul ebx
+        mov y2, eax
+        jmp ifend2
+    else2:
+        mov y2, 11
+    ifend2:
+        mov eax, y1
+        mov ebx, y2
+        sub eax, ebx
+        mov y, eax
+    }
+    cout << a << x << " " << y1 << " " << y2 << " " << y;
+}
---
+++ C++ Programs/C++ Programs/AssemblyLab_1/AssemblyLab_1.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+
+
+
+int main()
+{
+    int y, a;
+    
+    __asm {
+        mov eax, 5
+        mov ebx, 3
+        mul ebx
+        mov ebx, eax
+        mov eax, 13
+        mov ecx, 4
+        sub edx, edx
+        div ecx
+        add eax, ebx
+        mov a, eax // Числитель
+        mov eax, 12
+        mov ecx, 5
+        sub edx, edx
+        div ecx
+        add eax, 1 // Знаменатель
+        mov ecx, eax
+        mov eax, a
+        sub edx, edx
+        div ecx
+        mov a, eax // первая дробь готова
+
+
+        mov eax, 13
+        mov ecx, 3
+        sub edx, edx
+        div ecx // Третья дробь готова
+
+        mov ebx, a
+        sub ebx, 28
+        add ebx, eax
+
+        mov y, ebx
+        
+    }
+
+    std::cout << y;
+}
